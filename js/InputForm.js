aaaa703393974e906db00759668362eaaf209646
Aj007ar <aj007.ar@gmail.com>
2022-10-16 11:24:08 +0530

    [Ajay] Added email validation

--- js/InputForm.js
+++ js/InputForm.js
@@ -8,3 +8,16 @@
             else
             textError.textContent="Invalid Name";
         });
+        //UC Email validation
+        const email=document.querySelector("#Email");
+        const emailError=document.querySelector('.email-error');
+        email.addEventListener('input',function()
+        {
+            let emailRegex=RegExp('^[a-z]+([.+-_][a-z]+)*@[a-z]+.[a-z]+([.]+[a-z]+)*$');
+            if (emailRegex.test(Email.value)) {
+                emailError.textContent = ""
+            }
+            else{
+                emailError.textContent = "Email is not valid"
+            }
+        });
